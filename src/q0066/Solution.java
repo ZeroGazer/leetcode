ab9ddd2e64045dcd38b661957a15bdd09048db13
Alex Poon <6593392+ZeroGazer@users.noreply.github.com>
2021-02-18 16:55:54 +0800

    Update q0066

--- src/q0066/Solution.java
+++ src/q0066/Solution.java
@@ -1,23 +1,29 @@
 package q0066;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int[] plusOne(int[] digits) {
-        List<Integer> d = new ArrayList<>(digits.length);
-        for (int i = 0; i < digits.length; i++) {
-            d.add(0);
+        int sum = digits[digits.length - 1] + 1;
+
+        if (sum == 10) {
+            int a = 1;
+            for (int i = digits.length - 1; i > -1; i--) {
+                int b = digits[i] + a;
+                digits[i] = b % 10;
+                a = b / 10;
+                if (a == 0) {
+                    return digits;
+                }
+            }
+
+            int[] d = new int[digits.length + 1];
+            d[0] = a;
+            for (int i = 0; i < digits.length; i++) {
+                d[i + 1] = digits[i];
+            }
+            return d;
+        } else {
+            digits[digits.length - 1] = digits[digits.length - 1] + 1;
+            return digits;
         }
-        int a = 1;
-        for (int i = digits.length - 1; i > -1; i--) {
-            int b = digits[i] + a;
-            d.set(i, b % 10);
-            a = b / 10;
-        }
-        if (a == 1) {
-            d.add(0, 1);
-        }
-        return d.stream().mapToInt(Integer::intValue).toArray();
     }
 }

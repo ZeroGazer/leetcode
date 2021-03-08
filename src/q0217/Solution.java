bdf9b61b535908981e9e5aa96c92dee00a2c6564
Alex Poon <6593392+ZeroGazer@users.noreply.github.com>
2021-03-08 16:02:55 +0800

    Add q0217

---
+++ src/q0217/Solution.java
@@ -0,0 +1,20 @@
+package q0217;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Boolean> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return true;
+            } else {
+                map.put(nums[i], true);
+            }
+        }
+
+        return false;
+    }
+}

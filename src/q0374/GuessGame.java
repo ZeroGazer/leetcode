5c7ff6e310c076a43b68c3625c82b71509d7273f
Alex Poon <6593392+ZeroGazer@users.noreply.github.com>
2021-04-11 21:03:07 +0800

    Add q0374

---
+++ src/q0374/GuessGame.java
@@ -0,0 +1,7 @@
+package q0374;
+
+class GuessGame {
+    int guess(int num) {
+        return 0; // dummy
+    }
+}
---
+++ src/q0374/Solution.java
@@ -0,0 +1,31 @@
+package q0374;
+
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int l = 1, r = n;
+
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            switch(guess(m)) {
+                case 0:
+                    return m;
+                case 1:
+                    l = m + 1;
+                    break;
+                case -1:
+                    r = m;
+                    break;
+            }
+        }
+        return r;
+    }
+}

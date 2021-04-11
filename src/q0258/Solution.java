807b6b16e8a970d56c4f1acdd9cc9f672ba8f95e
Alex Poon <6593392+ZeroGazer@users.noreply.github.com>
2021-04-11 18:20:19 +0800

    Add q0258

---
+++ src/q0258/Solution.java
@@ -0,0 +1,17 @@
+package q0258;
+
+class Solution {
+    public int addDigits(int num) {
+        int result = 0;
+        while (num > 0) {
+            result += num % 10;
+            num /= 10;
+
+            if (num == 0 && result > 9) {
+                num = result;
+                result = 0;
+            }
+        }
+        return result;
+    }
+}
